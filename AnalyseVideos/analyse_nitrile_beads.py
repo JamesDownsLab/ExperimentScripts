2c2714bba5153373d489abc33b4f291a6489fdff
James <jamesdownslab@gmail.com>
2019-04-15 09:05:44 +0100

    Added scripts to new repo

---
+++ AnalyseVideos/analyse_nitrile_beads.py
@@ -0,0 +1,51 @@
+from ParticleTracking import dataframes, configurations, tracking, statistics, annotation
+from Generic import filedialogs
+import warnings
+warnings.filterwarnings("ignore")
+import time
+
+### Load a file ###
+###################
+file = filedialogs.load_filename('Load a video', remove_ext=False, directory='/home/ppxjd3/Videos/')
+# file = "/home/ppxjd3/Videos/test_move.MP4"
+### Tracking ###
+###############
+methods = configurations.NITRILE_BEADS_METHOD
+options = configurations.NITRILE_BEADS_PARAMETERS
+
+pt = tracking.ParticleTracker(file, methods, options, multiprocess=False,
+                              crop_method='auto', show_debug=False)
+s = time.time()
+pt.track()
+print(time.time() - s)
+
+data_store = dataframes.DataStore(file, load=True)
+
+
+
+
+data_store.inspect_dataframes()
+
+### Annotations ###
+###################
+annotator = annotation.VideoAnnotator(data_store, file)
+annotator.add_coloured_circles()
+# # # annotation.neighbors(data_store, 0)
+
+### Statistics ###
+##################
+calculator = statistics.PropertyCalculator(data_store)
+# calculator.distance()
+# calculator.edge_distance()
+# calculator.level_checks()
+calculator.order()
+# calculator.susceptibility()
+# calculator.density()
+# calculator.average_density()
+# calculator.correlations(300, r_min=1, r_max=20, dr=0.04)
+# calculator.correlations(10)
+
+### Graphs ###
+##############
+
+# graphs.order_quiver(data_store, 0)
---
+++ RunEquipment/amplitude_ramps.py
@@ -0,0 +1,5 @@
+from Shaker import power
+
+PS = power.PowerSupply()
+PS.ramp(900, 500, 1)
+PS.quit()
