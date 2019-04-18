0534ce23d4a8d11017b89e0435a07a0dae85bd91
James <jamesdownslab@gmail.com>
2019-04-18 15:34:46 +0100

    Removed unused import

--- AnalyseVideos/analyse_nitrile_beads.py
+++ AnalyseVideos/analyse_nitrile_beads.py
@@ -15,28 +15,29 @@ options = configurations.NITRILE_BEADS_PARAMETERS
 pt = tracking.ParticleTracker(file, options, multiprocess=False,
                               crop_method='auto', show_debug=False)
 # s = time.time()
-pt.track()
+# pt.track()
 # print(time.time() - s)
 #
 data_store = dataframes.DataStore(file, load=True)
-data_store.inspect_dataframes()
+# data_store.inspect_dataframes()
 
 ### Annotations ###
 ###################
 # annotator = annotation.VideoAnnotator(data_store, file)
 # annotator.add_coloured_circles()
 # # # annotation.neighbors(data_store, 0)
-annotator = video.CircleAnnotate(data_store, file)
+# annotator = video.CircleAnnotate(data_store, file)
 
 ### Statistics ###
 ##################
-# calculator = statistics.PropertyCalculator(data_store)
+calculator = statistics.PropertyCalculator(data_store)
+# print(data_store.num_frames)
 # calculator.distance()
 # calculator.edge_distance()
 # calculator.level_checks()
 # calculator.order()
 # calculator.susceptibility()
-# calculator.density()
+calculator.density()
 # calculator.average_density()
 # calculator.correlations(300, r_min=1, r_max=20, dr=0.04)
 # calculator.correlations(10)
