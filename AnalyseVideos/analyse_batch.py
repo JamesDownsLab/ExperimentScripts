9db9acc0abc6e7d1cfb02b1324ebd525574aaa0b
James <jamesdownslab@gmail.com>
2019-05-10 09:55:43 +0100

    Script to batch analyse videos

---
+++ AnalyseVideos/analyse_batch.py
@@ -0,0 +1,27 @@
+import warnings
+import os
+
+from Generic import filedialogs
+from ParticleTracking import dataframes, statistics, annotation
+from ParticleTracking.tracking.james_nitrile import JamesPT
+
+warnings.filterwarnings('ignore')
+
+directory = filedialogs.open_directory('Open Directory containing videos')
+files = os.listdir(directory)
+
+for file in files:
+    file = directory + '/' + file
+    name, ext = os.path.splitext(file)
+    if ext == '.hdf5':
+        print(ext)
+        continue
+    data_file = name + '.hdf5'
+    if not os.path.exists(data_file):
+        tracker = JamesPT(file, tracking=True, multiprocess=True)
+        tracker.track()
+    data_store = dataframes.DataStore(file, load=True)
+    # annotation.CircleAnnotator(file, data_store, 'particle').annotate()
+    calculator = statistics.PropertyCalculator(data_store)
+    calculator.order(multiprocessing=True)
+
