e6b75929fa69b8e328bbfd44ce835876197d822e
James <jamesdownslab@gmail.com>
2019-05-10 11:39:25 +0100

    Changed with to close and join

--- RunEquipment/amplitude_ramps.py
+++ RunEquipment/amplitude_ramps.py
@@ -1,5 +1,14 @@
 from Shaker import power
 
-PS = power.PowerSupply()
-PS.ramp(900, 500, 1)
-PS.quit()
+rates = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for rate in rates:
+
+    PS = power.PowerSupply()
+    PS.ramp(400, 1000, rate, 1)
+    PS.quit()
+
+    PS = power.PowerSupply()
+    PS.ramp(1000, 400, rate, 1)
+    PS.quit()
+
