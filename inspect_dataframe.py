dfb0159845e4cbc8d99458a7b8075e21e5813252
James <jamesdownslab@gmail.com>
2019-06-04 11:42:59 +0100

    Added Canny Gui

--- inspect_dataframe.py
+++ inspect_dataframe.py
@@ -7,3 +7,4 @@ file = filedialogs.load_filename(directory='/media/data/Data',
                                  file_filter='*.hdf5')
 data = dataframes.DataStore(file)
 print(data.df.head())
+print(data.df.groupby('frame').count())
