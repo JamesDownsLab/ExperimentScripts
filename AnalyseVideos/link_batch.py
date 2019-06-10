5bcc1ed72e694078402821b9a77993f7556aaf1b
James <jamesdownslab@gmail.com>
2019-06-10 16:35:07 +0100

    Moved linking to separate file

--- AnalyseVideos/link_batch.py
+++ AnalyseVideos/link_batch.py
@@ -1,5 +1,10 @@
 from Generic import filedialogs
 from ParticleTracking import linking, configurations
+import logging
+
+ch = logging.StreamHandler()
+formatter = logging.Formatter('\x1b[80D\x1b[1A\x1b[K%(message)s')
+ch.setFormatter(formatter)
 
 directory = filedialogs.open_directory('')
 
@@ -7,6 +12,7 @@ files = filedialogs.get_files_directory(directory+'/*.hdf5')
 config = configurations.TRACKPY_NITRILE_PARAMETERS
 
 for file in files:
+    print(file)
     linker = linking.Linker(file)
     linker.link(config['search_range'], memory=config['memory'])
     linker.quit()
