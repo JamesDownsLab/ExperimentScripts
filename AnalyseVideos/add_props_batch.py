01c00e59bf3650efac5447308904a35ec8c23967
James <jamesdownslab@gmail.com>
2019-07-23 14:36:34 +0100

    udpate

--- AnalyseVideos/add_props_batch.py
+++ AnalyseVideos/add_props_batch.py
@@ -10,9 +10,15 @@ directory = filedialogs.open_directory('Open Directory containing videos')
 files = filedialogs.get_files_directory(directory + '/*.hdf5')
 
 for file in files:
-    with dataframes.DataStore(file) as data:
-        # annotation.CircleAnnotator(file, data, 'particle').annotate()
-        calculator = statistics.PropertyCalculator(data)
-        # calculator.order()
-        calculator.density()
-        calculator.count()
+    if 'corr' not in file:
+        print(file)
+        meta = dataframes.load_metadata(file)
+        headings = meta['headings']
+        if 'edge_distance' not in headings:
+            with dataframes.DataStore(file) as data:
+                # annotation.CircleAnnotator(file, data, 'particle').annotate()
+                calculator = statistics.PropertyCalculator(data)
+                # calculator.order()
+                # calculator.density()
+                # calculator.count()
+                calculator.distance()
